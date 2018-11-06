4a221bd8698366d8febcb962ef6ade0b14b8801b
elly <elly.chan.mda@gmail.com>
2018-11-06 17:13:40 +0000

    add hash

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
@@ -7,6 +7,8 @@ class Oystercard
     @balance = 0
     @in_use = false
     @entry_station = entry_station
+    @exit_station = exit_station
+    @journey = {}
   end
 
   def top_up(amount)
@@ -17,12 +19,16 @@ class Oystercard
   def touch_in(station)
     @in_use = true if @in_use == false
     fail "Insufficient funds for journey" if @balance < MINIMUM_FARE
+    @journey.store(:entry_station, station)
     @entry_station = station
+    return station
   end
 
-  def touch_out
+  def touch_out(station)
     @in_use = false if @in_use == true
     deduct(MINIMUM_FARE)
+    @journey.store(:exit_station, station)
+    @exit_station = station
     @entry_station = nil
 	end
 
@@ -30,6 +36,10 @@ class Oystercard
   	entry_station
     #entry_station variable is always empty (nil) since nothing is assigned
     # !nil return true; !!nil return false
+  end
+
+  def journey
+    @journey
   end
 
 private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,6 +9,7 @@ describe Oystercard do
 	end
 
 	describe "#top_up" do
+
 		it "tops up the balance of the oystercard" do
 			expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
 		end
@@ -30,41 +31,52 @@ describe Oystercard do
 		end
 
 		it "deducts the correct fair from balance" do
-			subject.touch_out
+			station = Oystercard.new
+			subject.touch_out(station)
 			expect(subject.balance).to eq(4)
 		end
 
 		it "make sure the journey has been paid for" do
-			expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
+			station = Oystercard.new
+			expect{ subject.touch_out(station) }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
 		end
 
 	end
 
 	describe "#in-out journey" do
 
-		let (:station) {double :station}
+		let (:station1) {double :xxx}
+		let (:station2) {double :yyy}
 
 		it "checks if you are in journey after touched in" do
 			subject.top_up(5)
-			subject.touch_in(station)
+			subject.touch_in(station1)
 			expect(subject).to be_in_journey
 		end
 		it "check if you are not in journey after touch out" do
 			subject.top_up(5)
-			subject.touch_in(station)
-			subject.touch_out
+			subject.touch_in(station1)
+			subject.touch_out(station2)
 			expect(subject).to_not be_in_journey
 		end
 		it "ensures the card has sufficient balance for journey" do
 			subject.top_up(0)
-			expect{ subject.touch_in(station) }.to raise_error "Insufficient funds for journey"
+			expect{ subject.touch_in(station1) }.to raise_error "Insufficient funds for journey"
 		end
 
-		it "accept station when touch in" do
+		it "accept station when touch out" do
 			subject.top_up(5)
-			subject.touch_in(station)
-			expect(subject.entry_station).to eq station
+			subject.touch_in(station1)
+			subject.touch_out(station2)
+			expect(subject.exit_station).to eq station2
 		end
+
+		it "store the entery and exit station" do
+			subject.top_up(5)
+			subject.touch_in(station1)
+			expect(subject.journey).to include(:entry_station  => station1)
+		end
+
 	end
 
 end
