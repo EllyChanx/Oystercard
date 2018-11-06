f1035aaec1b12e3c7718d943fb970c218dae687e
elly <elly.chan.mda@gmail.com>
2018-11-06 14:13:36 +0000

    add entry station attr

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -25,36 +25,46 @@ describe Oystercard do
 
 	describe "#deduct" do
 
+		before(:each) do
+				subject.top_up(5)
+		end
+
 		it "deducts the correct fair from balance" do
-			subject.top_up(5)
 			subject.touch_out
 			expect(subject.balance).to eq(4)
 		end
 
 		it "make sure the journey has been paid for" do
-			subject.top_up(5)
 			expect{ subject.touch_out }.to change{ subject.balance }.by -Oystercard::MINIMUM_FARE
 		end
 
 	end
 
-	describe "# in-out journey" do
+	describe "#in-out journey" do
+
+		let (:station) {double :station}
+
 		it "checks if you are in journey after touched in" do
 			subject.top_up(5)
-			subject.touch_in
+			subject.touch_in(station)
 			expect(subject).to be_in_journey
 		end
 		it "check if you are not in journey after touch out" do
 			subject.top_up(5)
-			subject.touch_in
+			subject.touch_in(station)
 			subject.touch_out
 			expect(subject).to_not be_in_journey
 		end
 		it "ensures the card has sufficient balance for journey" do
 			subject.top_up(0)
-			expect{ (subject.touch_in) }.to raise_error "Insufficient funds for journey"
+			expect{ subject.touch_in(station) }.to raise_error "Insufficient funds for journey"
+		end
+
+		it "accept station when touch in" do
+			subject.top_up(5)
+			subject.touch_in(station)
+			expect(subject.entry_station).to eq station
 		end
 	end
 
-	
 end
